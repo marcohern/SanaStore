5a0297a48b956794c4f41275ebd67ba9f0f5fbf2
Marco Hernandez <marcohern@gmail.com>
2018-05-26 13:50:32 -0500

    improved UI a bit

--- ClientApp/app/components/settings/settings.component.ts
+++ ClientApp/app/components/settings/settings.component.ts
@@ -2,30 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../../services/settings.service';
 import { Settings } from '../../models/settings';
 
+class Mode {
+    id: number = 0;
+    name: string = '';
+}
+
 @Component({
     selector: 'products',
     templateUrl: './settings.component.html'
 })
 export class SettingsComponent implements OnInit {
 
-    mode: number = 0;
+    mode?: Mode;
+    modeval: number = 0;
+
+    modes:Mode[] = [
+        { id: 1, name: "Sql Server" },
+        { id: 2, name: "In Memory" }
+    ];
 
     constructor(private ss:SettingsService) { }
 
     ngOnInit() {
         this.ss.get().subscribe(settings => {
             console.log(settings);
-            this.mode = settings.sourceType;
+            this.mode = this.modes.find(m => m.id == settings.sourceType) || { id: 0, name: "" };
+            this.modeval = this.mode.id;
         }, error => {
 
         });
     }
 
     apply() {
-        console.log(this.mode);
-        this.ss.apply(this.mode).subscribe(result => {
-            console.log(result);
-        }, error => {
-        });
+        console.log(this.modeval);
+        if (this.modeval) {
+            this.ss.apply(this.modeval).subscribe(result => {
+                this.mode = this.modes.find(m => m.id == this.modeval) || { id: 0, name: "" };
+                console.log(result);
+            }, error => {
+            });
+        }
     }
 }
