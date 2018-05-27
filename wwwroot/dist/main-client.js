ef038e6ee46ac9a1b47df0f3f2fa055e79e2ecb2
Marco Hernandez <marcohern@gmail.com>
2018-05-26 19:09:52 -0500

    latest build

--- wwwroot/dist/main-client.js
+++ wwwroot/dist/main-client.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "fa316067e3d991d7ee6e"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "586681708af80a4bf234"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
